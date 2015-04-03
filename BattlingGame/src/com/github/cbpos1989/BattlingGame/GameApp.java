88ce08551db2cbac78fb6b86bfcfff8871ba2366
Colm O'Sullivan <cbpos1989@gmail.com>
2015-04-03 20:26:27 +0100

    Initial Commit

---
+++ BattlingGame/src/com/github/cbpos1989/BattlingGame/GameApp.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package com.github.cbpos1989.BattlingGame;
+
+/**
+ * @author Colm O'Sullivan
+ *
+ */
+public class GameApp {
+	public static void main (String[] args){
+		System.out.println("Working");
+	}
+}
